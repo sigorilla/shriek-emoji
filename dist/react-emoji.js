8aac75d9b629b564840f695ac6d57f1c75b38654
Igor Stepanov <sigorilla@gmail.com>
2015-10-10 20:14:05 +0300

    add dist

--- dist/react-emoji.js
+++ dist/react-emoji.js
@@ -1,4 +1,54 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/*!
+  Copyright (c) 2015 Jed Watson.
+  Licensed under the MIT License (MIT), see
+  http://jedwatson.github.io/classnames
+*/
+/* global define */
+
+(function () {
+	'use strict';
+
+	var hasOwn = {}.hasOwnProperty;
+
+	function classNames () {
+		var classes = '';
+
+		for (var i = 0; i < arguments.length; i++) {
+			var arg = arguments[i];
+			if (!arg) continue;
+
+			var argType = typeof arg;
+
+			if (argType === 'string' || argType === 'number') {
+				classes += ' ' + arg;
+			} else if (Array.isArray(arg)) {
+				classes += ' ' + classNames.apply(null, arg);
+			} else if (argType === 'object') {
+				for (var key in arg) {
+					if (hasOwn.call(arg, key) && arg[key]) {
+						classes += ' ' + key;
+					}
+				}
+			}
+		}
+
+		return classes.substr(1);
+	}
+
+	if (typeof module !== 'undefined' && module.exports) {
+		module.exports = classNames;
+	} else if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
+		// register as 'classnames', consistent with npm package name
+		define('classnames', function () {
+			return classNames;
+		});
+	} else {
+		window.classNames = classNames;
+	}
+}());
+
+},{}],2:[function(require,module,exports){
 var EmojiBtn = React.createClass({displayName: "EmojiBtn",
   getInitialState: function () {
     return { showEmojiMenu : false };
@@ -65,13 +115,24 @@ var EmojiMenu = React.createClass({displayName: "EmojiMenu",
     area.focus();
   },
   render: function () {
-    var self = this;
-    var classes = this.props.show ? 'emoji-menu active' : 'emoji-menu';
+    var _this = this;
+    var cx = require('classnames');
+    var outerKey = 0;
+
+    var classesEmojiMenu = cx({
+      'emoji-menu': true,
+      'active': this.props.show
+    });
+    var classesEmojiItems = cx({
+      'emoji': true,
+    });
+
     return (
-      React.createElement("div", {className: classes}, 
+      React.createElement("div", {className: classesEmojiMenu}, 
          this.props.items.map(function(value){
-          var classes = 'emoji emoji-' + value;
-          return React.createElement("span", {className: classes, onClick: self.addEmoji.bind(self, value)});
+          outerKey++;
+          return React.createElement("span", {className: classesEmojiItems + ' emoji-' + value, o: true, 
+          nClick: _this.addEmoji.bind(_this, value), key: outerKey});
         }) 
       )
     )
@@ -88,4 +149,4 @@ if (window.registerMessagePlugin !== undefined) {
   window.registerMessagePlugin(EmojiBtn);
 }
 
-},{}]},{},[1]);
+},{"classnames":1}]},{},[2]);
